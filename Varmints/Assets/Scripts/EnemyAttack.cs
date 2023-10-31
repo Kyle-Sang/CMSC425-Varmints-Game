d9cbaca9fc173744d89107f2931ac0c236c88141
Kyle-Sang <78330032+Kyle-Sang@users.noreply.github.com>
2023-10-31 03:57:25 -0400

    Wall health and player health implementations

--- Varmints/Assets/Scripts/EnemyAttack.cs
+++ Varmints/Assets/Scripts/EnemyAttack.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public float attackSpeed;
+    public int damage;
+    private float delay = 0.0f;
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag != "Enemy" && other.TryGetComponent(out IDamageable damageable) && Time.time > delay) {
+                damageable.Damage(damage, new Vector3(0, 0, 0));
+                delay += attackSpeed;
+            }
     }
 }
