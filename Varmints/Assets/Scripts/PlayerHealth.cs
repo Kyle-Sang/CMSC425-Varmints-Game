8a7fb269feba365b39d1f6d8110526fb9f68f1e0
Gibson Murray <gibmurrays@gmail.com>
2023-10-31 16:39:48 -0400

    Added Pause Menu + Game Over Menu
    still needs to work with mouse

--- Varmints/Assets/Scripts/PlayerHealth.cs
+++ Varmints/Assets/Scripts/PlayerHealth.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     public int health = 100;
+    public GameObject gameOverMenu;
 
     public void Damage(int damage, Vector3 force)
     {
         health -= damage;
         if (health <= 0)
         {
-            Debug.Log("I DIED");
+            gameOverMenu.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 }
