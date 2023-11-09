803b1640b8b4fad729a8ee3b73327f562d8b0e15
gzhang62 <garyzhang62@gmail.com>
2023-11-09 16:46:31 -0500

    updates from mac

--- Varmints/Assets/TargetClosest.cs
+++ Varmints/Assets/TargetClosest.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TargetClosest : MonoBehaviour
 {
+    private List<GameObject> enemies;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = GameObject.Find("WaveSpawner").GetComponent<WaveSpawner>().enemies;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform target = FindClosest();
+        transform.LookAt(target);
     }
+
+    private Transform FindClosest() 
+    {
+        return enemies.Min(enemy => GetDistance(enemy));
+    }
+
+    private float GetDistance(GameObject enemy) 
+    {
+        return Vector3.Distance(enemy, this.transform.position);
+    }
+
 }
